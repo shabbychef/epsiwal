8b3ceda4de538e8ae75827f699536ea6e468ed58
Steven Pav <shabbychef@gmail.com>
2019-06-12 21:14:03 -0700

    adding ref to lee et al.

---
+++ man-roxygen/ref-lee.R
@@ -0,0 +1,6 @@
+#' @references
+#'
+#' Lee, J. D., Sun, D. L., Sun, Y. and Taylor, J. E. "Exact post-selection inference, 
+#' with application to the Lasso." Ann. Statist. 44, no. 3 (2016): 907-927.
+#' doi:10.1214/15-AOS1371. \url{https://arxiv.org/abs/1311.6238}
+#'
