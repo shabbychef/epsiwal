8dd84575d35b881aa1103b068ac510b6b2fffa47
Steven Pav <shabbychef@gmail.com>
2026-06-09 09:59:01 -0700

    more CRAN nonsense

--- man-roxygen/ref-pav.R
+++ man-roxygen/ref-pav.R
@@ -1,5 +1,7 @@
 #' @references
 #'
 #' Pav, S. E. "Conditional inference on the asset with maximum Sharpe ratio."
-#' Arxiv e-print (2019). \url{http://arxiv.org/abs/1906.00573}
+#' Arxiv e-print (2019). \url{https://arxiv.org/abs/1906.00573}
 #'
+#' Pav, S. E.  "Post selection estimation of Sharpe ratios."
+#' Arxiv e-print (2026). \url{https://arxiv.org/abs/2606.01650}
